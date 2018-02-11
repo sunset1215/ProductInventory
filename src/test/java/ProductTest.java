53bed5c9befdf50ff626a2f0dae0353060d24bc7
Vivian Low <vivian198912@gmail.com>
2018-02-11 11:25:47 +0800

    Add test (failed) - auto increment product id

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -9,5 +9,13 @@ public class ProductTest {
         Assert.assertEquals(10, p.getId());
     }
     
+    @Test
+    public void testAutoIncrementId() {
+        Product p1 = new Product();
+        Assert.assertEquals(1, p1.getId());
+        Product p2 = new Product();
+        Assert.assertEquals(2, p2.getId());
+    }
+    
     
 }
