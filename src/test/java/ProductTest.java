825eb21d88a587fb041170f68f14aac9207e94bb
Vivian Low <vivian198912@gmail.com>
2018-02-11 11:51:30 +0800

    Add logger

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,11 +1,16 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProductTest {
+    
+    private static final Logger logger = LoggerFactory.getLogger(ProductTest.class);
     
     @Test
     public void testShouldStoreId() {
         Product p = new Product();
+        logger.debug("id={}", p.getId());
         Assert.assertTrue(p.getId() > 0);
     }
     
@@ -13,6 +18,7 @@ public class ProductTest {
     public void testAutoIncrementId() {
         Product p1 = new Product();
         Product p2 = new Product();
+        logger.debug("p1.id={}, p2.id={}", p1.getId(), p2.getId());
         Assert.assertEquals(p1.getId() + 1, p2.getId());
     }
     
