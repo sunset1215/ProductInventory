086511488af32319faecae4692b2030ac7a401e3
Vivian Low <vivian198912@gmail.com>
2018-02-12 23:24:13 +0800

    refactor

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,51 +11,54 @@ import static org.junit.Assert.assertTrue;
 public class ProductTest {
     
     private static final Logger logger = LoggerFactory.getLogger(ProductTest.class);
+    private static final double DEFAULT_PRICE = 12.12;
+    private static final int DEFAULT_QUANTITY = 10;
+    
+    private Product p1;
+    private Product p2;
+    
+    @Before
+    public void setUp() {
+        p1 = new Product("p1");
+        p1.setPrice(new BigDecimal(DEFAULT_PRICE));
+        p1.setQuantity(DEFAULT_QUANTITY);
+        
+        p2 = new Product("p2");
+        p2.setPrice(new BigDecimal(DEFAULT_PRICE));
+        p2.setQuantity(DEFAULT_QUANTITY);
+    }
     
     @Test
     public void testShouldStoreId() {
-        Product p = new Product("p");
-        logger.debug("id={}", p.getId());
-        assertTrue(p.getId() > 0);
+        logger.debug("id={}", p1.getId());
+        assertTrue(p1.getId() > 0);
     }
     
     @Test
     public void testAutoIncrementId() {
-        Product p1 = new Product("p1");
-        Product p2 = new Product("p2");
         logger.debug("p1.id={}, p2.id={}", p1.getId(), p2.getId());
         assertEquals(p1.getId() + 1, p2.getId());
     }
     
     @Test
     public void testShouldStoreName() {
-        Product p = new Product("Lays");
-        logger.debug("name={}", p.getName());
-        assertEquals("Lays", p.getName());
+        logger.debug("name={}", p1.getName());
+        assertEquals("p1", p1.getName());
     }
     
     @Test
     public void testShouldStorePrice() {
-        Product p = new Product("Ice Cream");
-        BigDecimal price = new BigDecimal(0.50);
-        p.setPrice(price);
-        assertEquals(price, p.getPrice());
+        assertEquals(new BigDecimal(DEFAULT_PRICE), p1.getPrice());
     }
     
     @Test
     public void testSamePriceOnDifferentProductShouldBeEqual() {
-        Product p1 = new Product("p1");
-        p1.setPrice(new BigDecimal(1.50));
-        Product p2 = new Product("p2");
-        p2.setPrice(new BigDecimal(1.50));
         assertEquals(p1.getPrice(), p2.getPrice());
     }
     
     @Test
     public void testShouldStoreQuantity() {
-        Product p = new Product("p");
-        p.setQuantity(10);
-        assertEquals(10, p.getQuantity());
+        assertEquals(DEFAULT_QUANTITY, p1.getQuantity());
     }
     
     
